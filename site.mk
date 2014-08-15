425f024e2cd8b172f7ac54250f5faf8120a00f5a
DaCHRIS <dachrls0@googlemail.com>
2014-08-15 14:03:19 +0200

    Update site.mk

--- site.mk
+++ site.mk
@@ -5,6 +5,7 @@ GLUON_SITE_PACKAGES := \
         gluon-config-mode \
         gluon-ebtables-filter-multicast \
         gluon-ebtables-filter-ra-dhcp \
+        gluon-ath9k-workaround \
         gluon-luci-admin \
         gluon-luci-autoupdater \
         gluon-luci-portconfig \
