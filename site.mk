2fd820e76c484c8c4edc685f3a925a97756e0c73
Philip Berndroth <p.berndroth@philuweb.de>
2014-08-17 19:44:18 +0200

    Update site.mk
    remove "gluon-ath9k-workaround" after discussion with gluon dev team.
    prepare the Site changes for gluon v2014.4 (http://gluon.readthedocs.org/en/latest/releases/v2014.4.html#site-changes).

--- site.mk
+++ site.mk
@@ -1,16 +1,15 @@
 GLUON_SITE_PACKAGES := \
+        gluon-mesh-batman-adv \
         gluon-alfred \
         gluon-announced \
         gluon-autoupdater \
         gluon-config-mode \
         gluon-ebtables-filter-multicast \
         gluon-ebtables-filter-ra-dhcp \
-        gluon-ath9k-workaround \
         gluon-luci-admin \
         gluon-luci-autoupdater \
         gluon-luci-portconfig \
         gluon-next-node \
-        gluon-mesh-batman-adv \
         gluon-mesh-vpn-fastd \
         gluon-radvd \
         gluon-status-page \
@@ -19,7 +18,7 @@ GLUON_SITE_PACKAGES := \
         haveged
  
  
-DEFAULT_GLUON_RELEASE := 0.5-beta-2
+DEFAULT_GLUON_RELEASE := 0.5-beta-3
  
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
